ba499140631493224c0377e7b9e1ba684fb052c6
freetux <thefreetux@gmail.com>
2021-03-30 00:20:31 +0430

    Update ShamsiDate.java

--- TMessagesProj/src/main/java/org/telegram/freetux/utils/shamsicalendar/ShamsiDate.java
+++ TMessagesProj/src/main/java/org/telegram/freetux/utils/shamsicalendar/ShamsiDate.java
@@ -33,7 +33,7 @@ public class ShamsiDate implements Comparable<ShamsiDate> {
         this(i, i2, i3, i4, i5, 0);
     }
 
-    public ShamsiDate(int i, int i2, int i3, int i4, int i5, int i6) {
+    public ShamsiDate(int i, int i2, int i3, int i4, int i5, int i6) throws Exception {
         setYear(i);
         setMonth(i2);
         setDay(i3);
