b3375bd4e72ed36a67aa10f116886ea7414e6e52
freetux <thefreetux@gmail.com>
2021-03-29 14:18:09 +0430

    Update ShamsiDate.java

--- TMessagesProj/src/main/java/org/telegram/freetux/utils/shamsicalendar/ShamsiDate.java
+++ TMessagesProj/src/main/java/org/telegram/freetux/utils/shamsicalendar/ShamsiDate.java
@@ -1,144 +1,290 @@
 package org.telegram.freetux.utils.shamsicalendar;
 
-import android.annotation.SuppressLint;
-import android.support.v4.view.PointerIconCompat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
-@SuppressLint({"UseValueOf"})
-public class ShamsiCalendar {
-    public static final int CURRENT_CENTURY = 13;
-    public static final String[] shamsiMonths;
-    private static final String[] shamsiMonthsEn;
-    private static final String[] shamsiMonthsKur;
-    public static final String[] shamsiWeekDays;
-    public static final String[] shamsiWeekDaysKu;
-    private static final Map<Integer, String> shamsiWeekDaysKuMap;
-    private static final Map<Integer, String> shamsiWeekDaysMap;
+public class ShamsiDate implements Comparable<ShamsiDate> {
+    public static final Pattern SHAMSI_DATE_PATTERN;
+    public static final Pattern TIME_PATTERN;
+    private int day;
+    private int hour;
+    private int minute;
+    private int month;
+    private int second;
+    private int smallYear;
+    private int year;
 
     static {
-        shamsiWeekDays = new String[]{"\u0634\u0646\u0628\u0647", "\u06cc\u06a9\u0634\u0646\u0628\u0647", "\u062f\u0648\u0634\u0646\u0628\u0647", "\u0633\u0647 \u0634\u0646\u0628\u0647", "\u0686\u0647\u0627\u0631\u0634\u0646\u0628\u0647", "\u067e\u0646\u062c \u0634\u0646\u0628\u0647", "\u062c\u0645\u0639\u0647"};
-        shamsiWeekDaysKu = new String[]{"\u0634\u0647 \u0645\u06cc", "\u06cc\u0647 \u06a9 \u0634\u0647 \u0645", "\u062f\u0648 \u0634\u0647 \u0645", "\u0633\u06cc \u0634\u0647 \u0645", "\u0686\u0627\u0631\u0634\u0647 \u0645", "\u067e\u0646\u062c \u0634\u0647 \u0645", "\u0626\u06cc\u0646"};
-        shamsiMonths = new String[]{"\u0641\u0631\u0648\u0631\u062f\u06cc\u0646", "\u0627\u0631\u062f\u06cc\u0628\u0647\u0634\u062a", "\u062e\u0631\u062f\u0627\u062f", "\u062a\u06cc\u0631", "\u0645\u0631\u062f\u0627\u062f", "\u0634\u0647\u0631\u06cc\u0648\u0631", "\u0645\u0647\u0631", "\u0622\u0628\u0627\u0646", "\u0622\u0630\u0631", "\u062f\u06cc", "\u0628\u0647\u0645\u0646", "\u0627\u0633\u0641\u0646\u062f"};
-        shamsiMonthsEn = new String[]{"Farvardin", "Ordibehesht", "Khordad", "Tir", "Mordad", "Shahrivar", "Mehr", "Aban", "Azar", "Dey", "Bahman", "Esfand"};
-        shamsiMonthsKur = new String[]{"\u062e\u0627\u06a9\u0647\u200c\u0644\u06ce\u0648\u0647", "\u06af\u0648\u06b5\u0627\u0646", "\u062c\u06c6\u0632\u0647\u200c\u0631\u062f\u0627\u0646", "\u067e\u0648\u0634\u067e\u0647\u200c\u0631", "\u06af\u0647\u200c\u0644\u0627\u0648\u06ce\u0698", "\u062e\u0647\u200c\u0631\u0645\u0627\u0646\u0627\u0646", "\u0631\u0647\u200c\u0632\u0628\u0647\u200c\u0631", "\u06af\u0647\u200c\u06b5\u0627\u0631\u06ce\u0632\u0627\u0646", "\u0633\u0647\u200c\u0631\u0645\u0627\u0648\u0647\u200c\u0632", "\u0628\u0647\u200c\u0641\u0631\u0627\u0646\u0628\u0627\u0631", "\u0631\u06ce\u0628\u0647\u200c\u0646\u062f\u0627\u0646", "\u0631\u0647\u200c\u0634\u0647\u200c\u0645\u0647"};
-        shamsiWeekDaysMap = new HashMap();
-        shamsiWeekDaysKuMap = new HashMap();
-        shamsiWeekDaysMap.put(new Integer(7), shamsiWeekDays[0]);
-        shamsiWeekDaysMap.put(new Integer(1), shamsiWeekDays[1]);
-        shamsiWeekDaysMap.put(new Integer(2), shamsiWeekDays[2]);
-        shamsiWeekDaysMap.put(new Integer(3), shamsiWeekDays[3]);
-        shamsiWeekDaysMap.put(new Integer(4), shamsiWeekDays[4]);
-        shamsiWeekDaysMap.put(new Integer(5), shamsiWeekDays[5]);
-        shamsiWeekDaysMap.put(new Integer(6), shamsiWeekDays[6]);
-        shamsiWeekDaysKuMap.put(new Integer(7), shamsiWeekDaysKu[0]);
-        shamsiWeekDaysKuMap.put(new Integer(1), shamsiWeekDaysKu[1]);
-        shamsiWeekDaysKuMap.put(new Integer(2), shamsiWeekDaysKu[2]);
-        shamsiWeekDaysKuMap.put(new Integer(3), shamsiWeekDaysKu[3]);
-        shamsiWeekDaysKuMap.put(new Integer(4), shamsiWeekDaysKu[4]);
-        shamsiWeekDaysKuMap.put(new Integer(5), shamsiWeekDaysKu[5]);
-        shamsiWeekDaysKuMap.put(new Integer(6), shamsiWeekDaysKu[6]);
+        SHAMSI_DATE_PATTERN = Pattern.compile("0*1[34]\\d\\d[/\\\\-]0*([1-9]|1[012])[/\\\\-]0*([1-9]|[12]\\d|3[01])");
+        TIME_PATTERN = Pattern.compile("0*(|[12])\\d:0*(|[1-5])\\d(:(0*(|[1-5])\\d)|)");
     }
 
-    public static ShamsiDate dateToShamsi(Date date) {
-        int i;
-        int i2;
-        int i3;
-        long dSTSavings = (((long) (TimeZone.getDefault().inDaylightTime(date) ? TimeZone.getDefault().getDSTSavings() : 0)) + date.getTime()) - new Date("01/01/1900").getTime();
-        long j = dSTSavings / 1000;
-        int i4 = (int) (j % 60);
-        j /= 60;
-        int i5 = (int) (j % 60);
-        int i6 = (int) ((j / 60) % 24);
-        long j2 = dSTSavings / 86400000;
-        if (j2 <= 78) {
-            i = (short) ((int) (((10 + j2) / 30) + 10));
-            i2 = (short) ((int) (((j2 + 10) % 30) + 1));
-            i3 = 1278;
+    private ShamsiDate() {
+    }
+
+    public ShamsiDate(int i, int i2, int i3) {
+        this(i, i2, i3, 0, 0, 0);
+    }
+
+    public ShamsiDate(int i, int i2, int i3, int i4, int i5) {
+        this(i, i2, i3, i4, i5, 0);
+    }
+
+    public ShamsiDate(int i, int i2, int i3, int i4, int i5, int i6) {
+        setYear(i);
+        setMonth(i2);
+        setDay(i3);
+        setHour(i4);
+        setMinute(i5);
+        setSecond(i6);
+    }
+
+    private static void isDateValid(ShamsiDate shamsiDate) {
+        if (!ShamsiCalendar.dateToShamsi(ShamsiCalendar.shamsiToDate(shamsiDate)).toDateString().equals(shamsiDate.toDateString())) {
+            throw new ShamsiDateIllegalFormatException("Invalid shamsi date : " + shamsiDate);
+        }
+    }
+
+    public static ShamsiDate parseDate(String str) {
+        if (str != null && TtmlNode.ANONYMOUS_REGION_ID.equals(str)) {
+            throw new ShamsiDateIllegalFormatException("Bad time format: <null>");
+        } else if (SHAMSI_DATE_PATTERN.matcher(str).matches()) {
+            ShamsiDate shamsiDate = new ShamsiDate();
+            try {
+                StringTokenizer stringTokenizer = new StringTokenizer(str, " -/\\");
+                shamsiDate.setYear(Integer.parseInt(stringTokenizer.nextToken()));
+                shamsiDate.setMonth(Integer.parseInt(stringTokenizer.nextToken()));
+                shamsiDate.setDay(Integer.parseInt(stringTokenizer.nextToken()));
+                isDateValid(shamsiDate);
+                return shamsiDate;
+            } catch (Throwable e) {
+                throw new ShamsiDateIllegalFormatException("Bad shamsi date format: " + str, e);
+            }
         } else {
-            j = j2 - 78;
-            int i7 = 1279;
-            while (true) {
-                j2 = ((long) (i7 + 11)) % 33;
-                i3 = (j2 == 32 || j2 % 4 != 0) ? 0 : 1;
-                if (j <= ((long) (i3 + 365))) {
-                    break;
-                }
-                j -= (long) (i3 + 365);
-                i7 = (short) (i7 + 1);
-            }
-            short s;
-            if (j <= 186) {
-                i2 = (short) ((int) (((j - 1) % 31) + 1));
-                s = (short) ((int) (((j - 1) / 31) + 1));
-                i3 = i7;
-            } else {
-                i2 = (short) ((int) ((((j - 1) - 186) % 30) + 1));
-                s = (short) ((int) ((((j - 1) - 186) / 30) + 7));
-                i3 = i7;
-            }
+            throw new ShamsiDateIllegalFormatException("Bad shamsi date format: " + str);
         }
-        return new ShamsiDate(i3, i, i2, i6, i5, i4);
     }
 
-    public static int getDaysInMonth(ShamsiDate shamsiDate) {
-        if (shamsiDate.getMonth() < 7) {
-            return 31;
+    public static ShamsiDate parseTime(String str) {
+        if (str != null && TtmlNode.ANONYMOUS_REGION_ID.equals(str)) {
+            throw new ShamsiDateIllegalFormatException("Bad time format: <null>");
+        } else if (TIME_PATTERN.matcher(str).matches()) {
+            try {
+                ShamsiDate shamsiDate = new ShamsiDate();
+                StringTokenizer stringTokenizer = new StringTokenizer(str, " :-/");
+                shamsiDate.setHour(Integer.parseInt(stringTokenizer.nextToken()));
+                shamsiDate.setMinute(Integer.parseInt(stringTokenizer.nextToken()));
+                if (stringTokenizer.hasMoreTokens()) {
+                    shamsiDate.setSecond(Integer.parseInt(stringTokenizer.nextToken()));
+                }
+                return shamsiDate;
+            } catch (Throwable e) {
+                throw new ShamsiDateIllegalFormatException("Bad time format: " + str, e);
+            }
+        } else {
+            throw new ShamsiDateIllegalFormatException("Bad time format: " + str);
         }
-        if (shamsiDate.getMonth() < 12) {
-            return 30;
+    }
+
+    private void setDay(int i) {
+        if (i < 1 || i > 31) {
+            throw new IllegalArgumentException("Day must be between 1 and 31");
         }
-        Date shamsiToDate = shamsiToDate(new ShamsiDate(shamsiDate.getYear(), shamsiDate.getMonth(), 29));
+        this.day = i;
+    }
+
+    private void setHour(int i) {
+        if (i < 0 || i >= 24) {
+            throw new IllegalArgumentException("Hour must be between 0 and 23");
+        }
+        this.hour = i;
+    }
+
+    private void setMinute(int i) {
+        if (i < 0 || i >= 60) {
+            throw new IllegalArgumentException("Minute must be between 0 and 59");
+        }
+        this.minute = i;
+    }
+
+    private void setMonth(int i) {
+        if (i < 1 || i > 12) {
+            throw new IllegalArgumentException("Month must be between 1 and 12");
+        }
+        this.month = i;
+    }
+
+    private void setSecond(int i) {
+        if (i < 0 || i >= 60) {
+            throw new IllegalArgumentException("Second must be between 0 and 59");
+        }
+        this.second = i;
+    }
+
+    private void setSmallYear(int i) {
+        if (i >= 0) {
+            this.smallYear = i;
+            return;
+        }
+        throw new IllegalArgumentException("Small Year must be positive");
+    }
+
+    private void setYear(int i) {
+        if (i >= 0) {
+            this.year = i;
+            setSmallYear(i % 100);
+            return;
+        }
+        throw new IllegalArgumentException("Year must be positive");
+    }
+
+    public void add(int i, int i2) {
+        int i3 = 30;
+        if (i == 2) {
+            this.month += i2;
+            if (this.month <= 0) {
+                this.month += 12;
+                this.year--;
+            } else if (this.month > 12) {
+                this.month -= 12;
+                this.year++;
+            }
+            if (this.month < 7 && this.day > 31) {
+                this.day = 31;
+            } else if (this.month < 12 && this.day > 30) {
+                this.day = 30;
+            } else if (this.month == 12 && this.day > 29) {
+                Date shamsiToDate = ShamsiCalendar.shamsiToDate(new ShamsiDate(this.year, this.month, 29));
+                Calendar instance = Calendar.getInstance();
+                instance.setTime(shamsiToDate);
+                instance.add(5, 1);
+                if (ShamsiCalendar.dateToShamsi(instance.getTime()).getMonth() != 12) {
+                    i3 = 29;
+                }
+                if (this.day > i3) {
+                    this.day = 29;
+                }
+            }
+        } else if (i == 1) {
+            this.year += i2;
+            this.year = this.year < 0 ? 0 : this.year;
+        } else {
+            Calendar instance2 = Calendar.getInstance();
+            instance2.setTime(ShamsiCalendar.shamsiToDate(this));
+            instance2.add(i, i2);
+            ShamsiDate dateToShamsi = ShamsiCalendar.dateToShamsi(instance2.getTime());
+            this.year = dateToShamsi.getYear();
+            this.month = dateToShamsi.getMonth();
+            this.day = dateToShamsi.getDay();
+        }
+    }
+
+    public int compareTo(ShamsiDate shamsiDate) {
+        if (shamsiDate != null) {
+            return this.year != shamsiDate.year ? this.year - shamsiDate.year : this.month != shamsiDate.month ? this.month - shamsiDate.month : this.day != shamsiDate.day ? this.day - shamsiDate.day : this.hour != shamsiDate.hour ? this.hour - shamsiDate.hour : this.minute != shamsiDate.minute ? this.minute - shamsiDate.minute : this.second != shamsiDate.second ? this.second - shamsiDate.second : 0;
+        } else {
+            throw new IllegalArgumentException("Object must not be null and must be of type ShamsiDate: " + shamsiDate);
+        }
+    }
+
+    public int getDay() {
+        return this.day;
+    }
+
+    public int getDayInYear() {
+        return this.month < 7 ? ((this.month - 1) * 31) + this.day : this.month <= 12 ? (((this.month - 7) * 30) + this.day) + 186 : 0;
+    }
+
+    public int getHour() {
+        return this.hour;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+    public int getMonth() {
+        return this.month;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public int getSmallYear() {
+        return this.smallYear;
+    }
+
+    public int getWeekOfYear() {
+        int i = 0;
+        Date shamsiToDate = ShamsiCalendar.shamsiToDate(new ShamsiDate(this.year, 1, 1));
+        int dayInYear = getDayInYear();
         Calendar instance = Calendar.getInstance();
         instance.setTime(shamsiToDate);
-        instance.add(5, 1);
-        return dateToShamsi(instance.getTime()).getMonth() != 12 ? 29 : 30;
-    }
-
-    public static String getShamsiMonth(int i) {
-        return (i >= 1 || i <= 12) ? shamsiMonths[i - 1] : null;
-    }
-
-    public static String getShamsiMonthEn(int i) {
-        return (i >= 1 || i <= 12) ? shamsiMonthsEn[i - 1] : null;
-    }
-
-    public static String getShamsiMonthKur(int i) {
-        return (i >= 1 || i <= 12) ? shamsiMonthsKur[i - 1] : null;
-    }
-
-    public static String[] getShamsiMonths() {
-        return shamsiMonths;
-    }
-
-    public static String getShamsiWeekDay(int i) {
-        return (i < 0 || i > 6) ? null : shamsiWeekDays[i];
-    }
-
-    public static String[] getShamsiWeekDays() {
-        return shamsiWeekDays;
-    }
-
-    public static Map<Integer, String> getShamsiWeekDaysKyMap() {
-        return shamsiWeekDaysKuMap;
-    }
-
-    public static Map<Integer, String> getShamsiWeekDaysMap() {
-        return shamsiWeekDaysMap;
-    }
-
-    public static Date shamsiToDate(ShamsiDate shamsiDate) {
-        int month = shamsiDate.getMonth() >= 7 ? (((shamsiDate.getMonth() - 7) * 30) + 186) + shamsiDate.getDay() : ((shamsiDate.getMonth() - 1) * 31) + shamsiDate.getDay();
-        long j = shamsiDate.getYear() == 1278 ? (long) (month - 287) : 79;
-        for (int i = 1279; i < shamsiDate.getYear(); i++) {
-            long j2 = ((long) (i + 11)) % 33;
-            Object obj = (j2 == 32 || j2 % 4 != 0) ? null : 1;
-            j = obj == 1 ? j + 366 : j + 365;
+        int i2 = instance.get(7);
+        int i3 = 0;
+        while (i2 != 7) {
+            i2 = i3 + 1;
+            instance.add(6, 1);
+            int i4 = i2;
+            i2 = instance.get(7);
+            i3 = i4;
         }
-        Date date = new Date(new Date("01/01/1900").getTime() + ((((((long) month) + j) - 1) * 86400000) + ((long) ((((shamsiDate.getHour() * 3600) * PointerIconCompat.TYPE_DEFAULT) + ((shamsiDate.getMinute() * 60) * PointerIconCompat.TYPE_DEFAULT)) + (shamsiDate.getSecond() * PointerIconCompat.TYPE_DEFAULT)))));
-        return TimeZone.getDefault().inDaylightTime(date) ? new Date(date.getTime() - ((long) TimeZone.getDefault().getDSTSavings())) : date;
+        i2 = i3 > 0 ? 1 : 0;
+        if (dayInYear <= i3) {
+            return i2;
+        }
+        i3 = dayInYear - i3;
+        dayInYear = i3 / 7;
+        if (i3 % 7 != 0) {
+            i = 1;
+        }
+        return (i + dayInYear) + i2;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String toDateString() {
+        return toDateString('/');
+    }
+
+    public String toDateString(char c) {
+        return String.valueOf(getYear()) + c + getMonth() + c + getDay();
+    }
+
+    public String toReverseDateString() {
+        return toReverseDateString('/');
+    }
+
+    public String toReverseDateString(char c) {
+        return String.valueOf(getDay()) + c + getMonth() + c + getYear();
+    }
+
+    public String toString() {
+        return getYear() + "/" + getMonth() + "/" + getDay() + " " + getHour() + ":" + getMinute() + ":" + getSecond();
+    }
+
+    public String toTimeString() {
+        return toTimeString(':');
+    }
+
+    public String toTimeString(char c) {
+        return toTimeString(c, false);
+    }
+
+    public String toTimeString(char c, boolean z) {
+        String str = (String.valueOf(getHour()) + c) + (getMinute() > 9 ? TtmlNode.ANONYMOUS_REGION_ID : "0") + getMinute();
+        if (!z) {
+            return str;
+        }
+        return str + c + (getSecond() > 9 ? TtmlNode.ANONYMOUS_REGION_ID : "0") + getSecond();
+    }
+
+    public String toTimeString(boolean z) {
+        return toTimeString(':', z);
     }
 }
